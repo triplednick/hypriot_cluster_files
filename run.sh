41eb58e835e98629d6ffdf731db6b4bd9873852d
Nick Stradford <triplednick@gmail.com>
2018-03-14 23:32:15 -0700

    updating run.sh to piont to local files

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-flash --bootconf /tmp/config.txt --userdata /tmp/user-data.yaml https://github.com/hypriot/image-builder-rpi/releases/download/v1.7.1/hypriotos-rpi-v1.7.1.img.zip
+flash --bootconf ./config.txt --userdata ./user-data.yaml https://github.com/hypriot/image-builder-rpi/releases/download/v1.7.1/hypriotos-rpi-v1.7.1.img.zip
